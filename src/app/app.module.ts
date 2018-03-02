c6d2abfa5f7c7f092224d4175c7da48924e27228
AMZURTECH\SudheerM <sudheer.meesala@amzurtech.com>
2018-03-02 21:55:01 +0530

    moduels added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import {ClinicalCommonModule} from './clinical-common/clinical-common.module';
+
 
 
 @NgModule({
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClinicalCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/clinical-common/clinical-common.module.ts
+++ src/app/clinical-common/clinical-common.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClinicalCommonRoutingModule } from './clinical-common-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ClinicalCommonRoutingModule
   ],
-  declarations: []
+  declarations: [HeaderComponent, FooterComponent, LeftMenuComponent, DashboardComponent]
 })
 export class ClinicalCommonModule { }
